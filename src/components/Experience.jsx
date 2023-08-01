efc33e62a18ed9ba434d1340bdb54c0dbb5facb4
SudaraWishvajith <it21221378@my.sliit.lk>
2023-08-01 16:26:03 +0530

    create CharacStage component
    create CharacStage component for create meshPortals for the charactors

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,27 +3,45 @@ import * as THREE from 'three'
 import { Wraith } from "./Wraith";
 
 export const Experience = () => {
-  const map = useTexture(
-    "textures/anime_horror_dark_world.jpg"
-  );
+  
 
   return (
     <>
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
-      <RoundedBox args={[2, 3, 0.1]}>
-        <MeshPortalMaterial>
+      <CharacStage texture={"textures/anime_horror_dark_world.jpg"}>
+         <Wraith scale={2} position-y={-2} />
+      </CharacStage>
+      <CharacStage texture={"textures/anime_lava_dark_world.jpg"} position-x={-2.5} rotation-y={Math.PI / 8 }>
+        
+      </CharacStage>
+      
+    </>
+  );
+};
+
+export const CharacStage = ({
+  children,
+  texture,
+  ...props
+}) => {
+  
+  const map = useTexture(texture);
+  return(
+    <group {...props}>
+       <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
           <ambientLight intensity={0.5} />
           <Environment preset="sunset" />
-          <Wraith scale={2} position-y={-2} />
+         {children}
           <mesh>
             <sphereGeometry args={[5, 64, 64]} />
             <meshStandardMaterial map={map} side={THREE.BackSide} />
           </mesh>
         </MeshPortalMaterial>
         </RoundedBox>
-
-    </>
-  );
-};
+    </group>
+   
+  )
+}
