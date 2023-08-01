325b00380ef2890950d3642a187c24791ccc1ca2
SudaraWishvajith <it21221378@my.sliit.lk>
2023-08-01 12:34:25 +0530

    add background textures

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from 'three'
 
 export const Experience = () => {
+  const map = useTexture(
+    "textures/anime_horror_dark_world.jpg"
+  );
+
   return (
     <>
+    <ambientLight intensity={0.5} />
+    <Environment preset="sunset" />
       <OrbitControls />
       <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+       <sphereGeometry args={[5, 32, 32]} />
+       <meshStandardMaterial map={map} side={THREE.BackSide} />
       </mesh>
     </>
   );
