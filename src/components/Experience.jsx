5e0b5e72dfa04242b244d5f79b1f6a982c3506f6
SudaraWishvajith <it21221378@my.sliit.lk>
2023-08-01 15:04:10 +0530

    add meshPortal

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
 import * as THREE from 'three'
+import { Wraith } from "./Wraith";
 
 export const Experience = () => {
   const map = useTexture(
@@ -8,13 +9,21 @@ export const Experience = () => {
 
   return (
     <>
-    <ambientLight intensity={0.5} />
-    <Environment preset="sunset" />
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
       <OrbitControls />
-      <mesh>
-       <sphereGeometry args={[5, 32, 32]} />
-       <meshStandardMaterial map={map} side={THREE.BackSide} />
-      </mesh>
+      <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial>
+          <ambientLight intensity={0.5} />
+          <Environment preset="sunset" />
+          <Wraith scale={2} position-y={-2} />
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+        </RoundedBox>
+
     </>
   );
 };
